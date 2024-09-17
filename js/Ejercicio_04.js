ca693d0259ffaaa9a7178b2ec5bbf409eed63fbb
Edwin Hernandez Campos <edwincampos803@gmail.com>
2024-09-17 15:54:42 -0600

    Practica 4
    Se finalizo el ejercicio 4, objetos , destructing, objeto literal enchament

---
+++ js/Ejercicio_04.js
@@ -0,0 +1,34 @@
+//objetos
+const nombre= "Tablet"
+const precio = 300
+const disponible = true
+
+const producto = {
+    nombre: "Tablet",
+    precio: 300,
+    disponible: true
+}
+console.log(producto)
+console.table(producto)
+
+console.log(producto.nombre)
+console.log(precio.nombre)
+console.log(disponible.nombre)
+
+//destructing
+
+const {nombre2, precio2, disponible2} = producto
+console.log(nombre)
+console.log(precio)
+console.log(disponible)
+
+//objeto literal enchament
+
+const autenticado = true
+const usuario = "edwin"
+const nuevoObjeto ={
+    autenticado : autenticado, 
+    usuario:usuario
+
+}
+console.table(nuevoObjeto)
