db22eff6295c90a54472782f451d7320ff175af7
Edwin Hernandez Campos <edwincampos803@gmail.com>
2024-09-17 15:32:42 -0600

    Practica 4
    ejercicio 3 tipos de datos , string, big Int y Symbol

---
+++ js/Ejercicio_03.js
@@ -0,0 +1,30 @@
+const alumno="Juan"
+let producto = "Monitor 20 pulgadas"
+
+const numero = "30"
+const numero2 = 30
+
+console.log (typeof numero)
+console.log (typeof numero2)
+
+
+const numeroGrande = BigInt (9182981948)
+console.log (typeof numeroGrande)
+
+const numeero = 10
+const numero4 = 20
+//console.log (numero = Number (numeroGrande))
+
+const numero6 ="30"
+const numero5 = 30
+console.log (typeof String  (numero))
+console.log (typeof Number(numero2))
+
+const primerSymbol =Symbol (30)
+const segundoSymbol = Symbol (30)
+console.log (primerSymbol === segundoSymbol)
+console.log (primerSymbol.valueOf())
+
+const descuento =null
+console.log(typeof descuento)
+
